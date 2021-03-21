c539097be3f329f94921dfe35caf638ef2a7763a
Markus Schwarz <ms@conspect.de>
2021-03-21 23:04:23 +0100

    change title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
+  pathPrefix: "/pwp",
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Welcome to PrettyWoodPrivacy`,
+    description: `Warn others about cameras in the woods which are violating the GDPR and file automated complaints about dumbass hunters.`,
+    author: `@conspect`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
